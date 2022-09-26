b4f1cb4bc8b465532c1afbfe03fec88c4b1d242d
Sunjida Akter <sanjidabintesayid7@gmail.com>
2022-09-27 02:26:14 +0600

    2nd pattern done

--- pattern.cpp
+++ pattern.cpp
@@ -36,3 +36,38 @@ output :
 
 */
 
+/*************************Number-2*****************************/
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int row=1;
+
+    while(row<=n){
+        int col=1;
+        while(col<=row){
+            cout<<"*";
+            col=col+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+
+    getch();
+}
+
+//input : 5
+/*
+output :
+
+*
+**
+***
+****
+*****
+
+*/
