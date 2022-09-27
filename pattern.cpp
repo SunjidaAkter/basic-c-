d8b65972fee45582dd8acb04f2e1274ce1f7d37a
Sunjida Akter <sanjidabintesayid7@gmail.com>
2022-09-28 01:11:11 +0600

    10th pattern done

--- pattern.cpp
+++ pattern.cpp
@@ -341,3 +341,83 @@ ABCDE
 
 
 */
+
+
+
+/*************************Number-10*****************************/
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int row=1;
+    int k=1;
+    while(row<=n){
+        int col=1;
+        while(col<=n){
+            char ch='A'+k-1;
+            cout<<ch;
+            col=col+1;
+            k=k+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+
+    getch();
+}
+
+//input : 5
+/*
+output :
+
+ABCDE
+FGHIJ
+KLMNO
+PQRST
+UVWXY
+
+
+*/
+
+
+
+/*************************Number-10*****************************/
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int row=1;
+    while(row<=n){
+        int col=1;
+        while(col<=n){
+            char ch='A'+row+col-2;
+            cout<<ch;
+            col=col+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+
+    getch();
+}
+
+//input : 5
+/*
+output :
+
+ABCDE
+BCDEF
+CDEFG
+DEFGH
+EFGHI
+
+
+*/
