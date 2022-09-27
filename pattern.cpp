9cc1de0c28b57f56dfe5e1f6a9f38fa27973321e
Sunjida Akter <sanjidabintesayid7@gmail.com>
2022-09-27 15:27:13 +0600

    7th pattern done

--- pattern.cpp
+++ pattern.cpp
@@ -225,3 +225,41 @@ output :
 
 
 */
+
+
+
+/*************************Number-7*****************************/
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int row=1;
+    while(row<=n){
+        int col=1;
+        while(col<=row){
+            cout<<row-col+1;
+            col=col+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+
+    getch();
+}
+
+//input : 5
+/*
+output :
+
+1
+21
+321
+4321
+54321
+
+
+*/
