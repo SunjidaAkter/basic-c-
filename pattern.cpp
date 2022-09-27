74d29ac9cf7c03074ee9b942422b41d82c91a6c0
Sunjida Akter <sanjidabintesayid7@gmail.com>
2022-09-27 14:36:45 +0600

    3rd pattern done

--- pattern.cpp
+++ pattern.cpp
@@ -71,3 +71,40 @@ output :
 *****
 
 */
+
+
+/*************************Number-3*****************************/
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int row=1;
+
+    while(row<=n){
+        int col=1;
+        while(col<=row){
+            cout<<row;
+            col=col+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+
+    getch();
+}
+
+//input : 5
+/*
+output :
+
+11111
+22222
+33333
+44444
+55555
+
+*/
