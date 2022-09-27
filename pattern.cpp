f4e5ad79106e2a92b53444254f248b012d81c4e4
Sunjida Akter <sanjidabintesayid7@gmail.com>
2022-09-28 01:18:28 +0600

    12th pattern done

--- pattern.cpp
+++ pattern.cpp
@@ -421,3 +421,39 @@ EFGHI
 
 
 */
+/*************************Number-11*****************************/
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int row=1;
+    while(row<=n){
+        int col=1;
+        while(col<=row){
+            char ch='A'+row-1;
+            cout<<ch;
+            col=col+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+
+    getch();
+}
+
+//input : 5
+/*
+output :
+
+A
+BB
+CCC
+DDDD
+EEEEE
+
+
+*/
