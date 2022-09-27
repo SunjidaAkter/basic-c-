6b712d25b4116e75210d263f5a4111b82cdcab75
Sunjida Akter <sanjidabintesayid7@gmail.com>
2022-09-28 01:24:08 +0600

    13th pattern done

--- pattern.cpp
+++ pattern.cpp
@@ -421,6 +421,9 @@ EFGHI
 
 
 */
+
+
+
 /*************************Number-12*****************************/
 #include<iostream>
 #include<conio.h>
@@ -454,6 +457,45 @@ BB
 CCC
 DDDD
 EEEEE
+
+
+*/
+
+
+
+/*************************Number-13*****************************/
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int row=1;
+    while(row<=n){
+        int col=1;
+        while(col<=row){
+            char ch='A'+row+col-2;
+            cout<<ch;
+            col=col+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+
+    getch();
+}
+
+//input : 5
+/*
+output :
+
+A
+BC
+CDE
+DEFG
+EFGHI
 
 
 */
